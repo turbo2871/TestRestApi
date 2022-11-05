e671193ef830828b85ebdab0ac50499f0ce3fa7c
dn280371svs1 <vladislav.sverchkov@privatbank.ua>
2022-11-05 12:15:57 +0200

    Add Api Structure

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
-print_r('api.local');
-exit;
+require dirname(__DIR__)."/inc/bootstrap.php";
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+require PROJECT_ROOT_PATH."/Controller/Api/UserController.php";
+
+$objFeedController = new UserController();
+$strMethodName = $uri[3] . 'Action';
+$objFeedController->{$strMethodName}();
