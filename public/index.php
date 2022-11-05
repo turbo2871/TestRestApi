c12c06ccc0ec8fdf174201a092c6892928ccb72f
dn280371svs1 <vladislav.sverchkov@privatbank.ua>
2022-11-05 15:47:16 +0200

    Refactor index.php

--- public/index.php
+++ public/index.php
@@ -4,13 +4,13 @@ require dirname(__DIR__)."/inc/bootstrap.php";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
+if ((isset($uri[1]) && $uri[1] != 'user') || !isset($uri[2])) {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
 
-require PROJECT_ROOT_PATH."/Controller/Api/UserController.php";
+require PROJECT_ROOT_PATH."/Controller/UserController.php";
 
 $objFeedController = new UserController();
-$strMethodName = $uri[3] . 'Action';
+$strMethodName = $uri[2] . 'Action';
 $objFeedController->{$strMethodName}();
